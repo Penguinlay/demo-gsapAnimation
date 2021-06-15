70b601e949ee1ef60f284a3d107d7fc4a2963e20
Shein Lin Phyo <sheinlinphyo@gmail.com>
2021-06-15 11:56:38 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# demo-gsapAnimation
+
+This is a simple project to imitate a [video animation](./assets/original.mp4) using [GSAP 3](https://greensock.com/3/).
+
+# Getting Started
+
+1. Clone the repo.
+2. Open `index.html` in your browser.
+3. Enjoy!
---
+++ script.js
@@ -0,0 +1,69 @@
+const viewportHeight = (n) => (n * Math.max(document.documentElement.clientHeight, window.innerHeight || 0)) / 100;
+
+const playAudio = () => {
+    const tl = gsap.timeline();
+    tl.call(() => new Audio("./assets/audio.mp3").play());
+    return tl;
+};
+
+const animatePerson1 = () => {
+    const tl = gsap.timeline();
+    tl.fromTo("#person-1", { x: -viewportHeight(25) }, { duration: 3, ease: "circ.out", x: viewportHeight(100) });
+    return tl;
+};
+
+const animatePerson2 = () => {
+    const tl = gsap.timeline();
+    tl.fromTo("#person-2", { x: -viewportHeight(25) }, { duration: 3, ease: "circ.out", x: viewportHeight(100) });
+    return tl;
+};
+
+const animatePerson3 = () => {
+    const tl = gsap.timeline();
+    tl.fromTo("#person-3", { x: -viewportHeight(25) }, { duration: 3, ease: "circ.out", x: viewportHeight(100) });
+    return tl;
+};
+
+const animatePeople = () => {
+    const tl = gsap.timeline();
+    tl.add(animatePerson1()).add(animatePerson2(), "-=2.5").add(animatePerson3(), "-=2.5");
+    return tl;
+};
+
+const animateText = () => {
+    const tl = gsap.timeline();
+
+    tl.to("#text", { duration: 0, opacity: 1 });
+
+    tl.fromTo("#text-line-1", { x: -viewportHeight(100) }, { duration: 0.9, ease: "circ.out", x: 10 });
+    tl.fromTo("#text-line-2", { x: -viewportHeight(100) }, { duration: 0.8, ease: "circ.out", x: 10 }, 0.1);
+    tl.fromTo("#text-line-3", { x: -viewportHeight(100) }, { duration: 0.7, ease: "circ.out", x: 10 }, 0.2);
+    tl.fromTo("#text-line-4", { x: -viewportHeight(100) }, { duration: 0.6, ease: "circ.out", x: 10 }, 0.3);
+
+    tl.to("#text-line-2", { duration: 0, fontSize: viewportHeight(4), x: 20, y: 20 }, 2);
+    tl.to("#text-line-1", { duration: 0, opacity: 0 }, 2.1);
+    tl.to("#text-line-3", { duration: 0, x: 45 }, 2.15);
+    tl.to("#text-line-2", { duration: 0, opacity: 0 }, 2.2);
+    tl.to("#text-line-3", { duration: 0, fontSize: viewportHeight(8), x: viewportHeight(8), y: -viewportHeight(18) }, 2.25);
+    tl.to("#text-line-4", { duration: 0, fontSize: viewportHeight(7), scaleX: 0.9, x: viewportHeight(6), y: -viewportHeight(18) }, 2.3);
+
+    return tl;
+};
+
+const animateBlocks = () => {
+    const tl = gsap.timeline();
+    tl.to("#top-block", { duration: 1, height: viewportHeight(10), y: -viewportHeight(10) });
+    tl.to("#bottom-block", { duration: 1, height: viewportHeight(10), y: -viewportHeight(10) });
+    return tl;
+};
+
+const animate = () => {
+    const tl = gsap.timeline();
+    tl.add(playAudio()).add(animatePeople()).add(animateText(), "-=2.5").add(animateBlocks());
+    return tl;
+};
+
+document.getElementById("play-icon").onclick = () => {
+    document.getElementById("play-icon").style.display = "none";
+    animate();
+};
